cd04472a73c9173a97bd8c3c06c75beaf887d845
Evan Owen <Johnwowen3@gmail.com>
2015-05-24 02:05:40 -0400

    undo
    Finished API user Tracking
    
    Ready For debugging

---
+++ StealthGame/Assets/Evan's Assets/Script/NightVision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NightVision : MonoBehaviour {
+	bool isNightVision = false;
+	public KeyCode TurnNightVisionOn;
+	float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (TurnNightVisionOn) && timer <= 0) {
+			if(isNightVision == true){
+							
+			}
+
+		}
+	}
+	void FixedUpdate(){
+
+	}
+}
---
+++ StealthGame/Assets/Jacob's Folder/FanSpin.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+function Update () {
+
+transform.Rotate(Vector3(0, 1, 0), 1);
+
+}
